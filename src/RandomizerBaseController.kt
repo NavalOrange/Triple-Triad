a2838999c4e1541f3db122ab9ee4900e8b409c27
Justin <justincasol@hotmail.com>
2018-03-10 13:59:03 -0800

    Okay, added MOST of the pictures. Colours seem okay, still gotta get a picture for water.

--- src/RandomizerBaseController.kt
+++ src/RandomizerBaseController.kt
@@ -5,19 +5,22 @@ import javafx.fxml.Initializable
 import javafx.geometry.Insets
 import javafx.scene.control.Button
 import javafx.scene.control.Label
+import javafx.scene.image.Image
 import javafx.scene.input.MouseEvent
 import javafx.scene.layout.*
 import javafx.scene.paint.Color
 import javafx.scene.paint.Paint
 import java.net.URL
 import java.util.*
+import javafx.scene.layout.BackgroundSize
+
+
 
 
 class RandomizerBaseController : Initializable {
     @FXML lateinit var view : VBox
 
-    @FXML private lateinit var grid : GridPane
-    @FXML private lateinit var button : Button
+    val pathToAssets = javaClass.classLoader.getResource(".")
 
     @FXML private lateinit var box1 : VBox
     @FXML private lateinit var box2 : VBox
@@ -50,21 +53,18 @@ class RandomizerBaseController : Initializable {
     private fun runRandomizer() {
         boxes.forEach {
             val element = getRandomElement()
-            it.style = "-fx-background-color: ${element.fillColour};";
+//            it.style = "-fx-background-color: ${element.fillColour};"
+            it.background = element.background
             val label = it.children.single() as Label
             label.text = element.text()
-            if (element == Element.Poison || element == Element.Earth) {
-                label.textFill = Color.WHITE
-            } else {
-                label.textFill = Color.BLACK
-            }
+            label.textFill = element.fontColour
         }
     }
 
     private fun getRandomElement() : Element {
         val randomNumber = Math.random()*100
         return when {
-            randomNumber < 72.0 -> Element.NonElemental
+            randomNumber < 72.0 -> Element.None
             randomNumber < 75.5 -> Element.Fire
             randomNumber < 79.0 -> Element.Ice
             randomNumber < 82.5 -> Element.Lightning
@@ -75,4 +75,28 @@ class RandomizerBaseController : Initializable {
             else -> Element.Wind
         }
     }
+}
+
+enum class Element(val fillColour: String, val fontColour : Color, private val textValue : String? = null) {
+    None("#FFFFFF", Color.BLACK, "Non-Elemental"),
+    Fire("#FF0000", Color.BLACK),
+    Ice("#b4cffa", Color.BLACK),
+    Lightning("#f0fb3e", Color.BLACK),
+    Earth("#613318", Color.WHITE),
+    Poison("#551A8B", Color.WHITE),
+    Water("#40a4df", Color.BLACK),
+    Holy("#FFE4E1", Color.BLACK),
+    Wind("#6ABF51", Color.BLACK);
+
+    private val image = Image(Element::class.java.getResource( "assets/$name.png" ).toString())
+    val background : Background
+
+    init {
+        val backgroundSize = BackgroundSize(100.0, 100.0, true, true, true, false)
+        val backgroundImage = BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize)
+        val backgroundColor = BackgroundFill(Paint.valueOf(fillColour), null, null)
+        background = Background(listOf(backgroundColor), listOf(backgroundImage))
+    }
+
+    fun text() = textValue ?: name
 }
